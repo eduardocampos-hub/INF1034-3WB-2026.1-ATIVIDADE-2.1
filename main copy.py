ad530274fa1360545942c50b90534042a9841a3e
eduardo <eduardo.campos@meuprovedor.com.br>
2026-03-25 12:43:00 -0300

    terminei o trabalho

--- main copy.py
+++ main copy.py
@@ -31,11 +31,11 @@ def triangulo(color1,x,y,tamanho):
         t.right(120) 
     t.end_fill()
     return
-tamanho = randint(10,100)
+tamanho1 = randint(10,100)
 x1 = randint(150,300)
 y1 = randint(150,300)
 color1 = textinput("Obter cor", "digite sua cor de preferência") 
-triangulo(color1,x1,y1,tamanho)
+triangulo(color1,x1,y1,tamanho1)
 
 
 
@@ -51,11 +51,11 @@ def octagono(x,y,color,tamanho):
         t.right(45)
     t.end_fill()
     return
-tamanho = randint(10,100)
+tamanho2 = randint(10,100)
 x2 = randint(-300,0)
 y2 = randint(200,300)
 color2 = textinput("Obter cor", "digite sua cor de preferência")
-octagono(x2,y2,color2,tamanho)
+octagono(x2,y2,color2,tamanho2)
 
 
 
@@ -73,10 +73,10 @@ def trapezio(cor,x,y,tamanho):
         t.right(45)
     t.end_fill()
 
-tamanho = randint(10,100)
+tamanho3 = randint(10,100)
 x3 = randint(150,300)
 y3 = randint(-200,-100)
-trapezio(color3,x3,y3,tamanho)
+trapezio(color3,x3,y3,tamanho3)
 
 
 # diamante
@@ -91,9 +91,9 @@ def diamante(cor,x,y,tamanho):
         t.fd(tamanho)
         t.right(90)
     t.end_fill()
-tamanho = randint(10,100)
+tamanho4 = randint(10,100)
 x4 = randint(-300,-150)
 y4 = randint(-300,-150)
-diamante(color4,x4,y4,tamanho)
+diamante(color4,x4,y4,tamanho4)
 
 mainloop()
